8b45bd4d42a0c697df7e13d1c188f626cfa9d280
Eyk Rehbein <eykrehbein@gmail.com>
2020-06-22 21:45:24 +0200

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,8 @@ export default ({ Component, pageProps }) => (
                 href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
                 rel="stylesheet"
             ></link>
+
+            <script src="/analytics.js"></script>
         </Head>
 
         <ThemeProvider>
